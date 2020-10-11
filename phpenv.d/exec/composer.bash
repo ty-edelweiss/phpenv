a2c94ffdafb1cb23a1f8353753a71c1961ce3db5
yusaku.b.takano <yusaku.b.takano@rakuten.com>
2020-10-11 09:44:23 +0900

    Fixed directory name

--- phpenv.d/exec/composer.bash
+++ phpenv.d/exec/composer.bash
@@ -4,9 +4,11 @@ set -e
 [ -n "$PHPENV_DEBUG" ] && set -x
 
 STATUS=0
-PHPENV_COMPSER_BIN_PATH=$(phpenv which composer 2>/dev/null) || STATUS="$?"
+PHPENV_COMPOSER_BIN_PATH=$(phpenv which composer 2>/dev/null) || STATUS="$?"
 
 if [[ "${STATUS}" == "0" && -z "${COMPOSER_HOME}" ]];
 then
-    export COMPOSER_HOME="${PHPENV_COMPSER_BIN_PATH%/bin*}"
+    PHPENV_VERSION_ROOT="${PHPENV_COMPOSER_BIN_PATH%/bin*}"
+    REAL_COMPOSER_HOME="${PHPENV_VERSION_ROOT}/.composer"
+    export COMPOSER_HOME="${REAL_COMPOSER_HOME}"
 fi
